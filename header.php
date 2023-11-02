2b0a595c4317c9a5b931aa8b0990b7d521aa9d7e
Rabbihasan610 <mmix00595@gmail.com>
2023-11-02 15:00:07 +0600

    some change

--- header.php
+++ header.php
@@ -1,1 +1,4 @@
-<?require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog.php");?>
+<?php
+    echo "Hello world!";
+    //require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog.php");
+?>
